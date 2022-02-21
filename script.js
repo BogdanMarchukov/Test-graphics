6d3557e35f347ff6533db8ebb4be70a4dc537d18
bogdan <bogdan_info@mail.ru>
2022-02-21 21:50:25 +0300

    Create class Block
    1 add function get
    2 add function addEventListener to button

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 
 
 const container = document.querySelector('.container')
-const blockOne = document.querySelector('.block-one')
-const blockTwo = document.querySelector('.block-two')
-const blockThree = document.querySelector('.block-three')
+
+class Block {
+    constructor(divName, buttonName) {
+        this.div = document.querySelector(`.${divName}`)
+        this.button = document.querySelector(`.${buttonName}`)
+    }
+
+    get(){
+        return {
+            div: this.div,
+            button: this.button
+        }
+    }
+
+    addRow(){
+       console.log(this.get())
+    }
+
+    eventClick() {
+        this.button.addEventListener('click', this.addRow.bind(this))
+    }
+
+}
+
+
+const blockOne = new Block('block-one', 'button-one')
+const blockTwo = new Block('block-two', 'button-two')
+
+const {button: buttonOne} = blockOne.get()
+const {button: buttonTwo} = blockTwo.get()
+
+console.log(blockOne.get(), 'block-one')
+
+blockOne.eventClick()
+blockTwo.eventClick()
+
+
+
 
 
