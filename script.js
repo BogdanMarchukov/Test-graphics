24d33e19a2919304a031a0b09cc2ebd8e5204111
bogdan <bogdan_info@mail.ru>
2022-02-21 19:38:28 +0300

    Beginning of work
    1 creating base files
    2 creating basic markup
    
    (cherry picked from commit c97617677682fa842deb5be6d28eef22d6b6fa53)

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const container = document.querySelector('.container')
+const blockOne = document.querySelector('.block-one')
+const blockTwo = document.querySelector('.block-two')
+const blockThree = document.querySelector('.block-three')
+
+
